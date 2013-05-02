e03d93edb9c7fd665b479ef79e7b502da098f775
Daniel Bimschas <daniel@bimschas.com>
2013-05-02 16:57:42 +0200

    first commit

---
+++ src/main/java/p3/LookHereBug.java
@@ -0,0 +1,16 @@
+package p3;
+
+import p1.*;
+
+public class LookHereBug {
+
+	public static void main(String[] args) {
+		ClassA a = new ClassA();
+		ClassB b = new ClassB();
+		ClassC c = new ClassC();
+		ClassD d = new ClassD();
+		ClassE e = new ClassE();
+		ClassF f = new ClassF();
+	}
+
+}
